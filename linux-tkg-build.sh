fad63bbd5b8009b2d3aa2160b02e79b8f46c8fcb
Raul Dipeas <rauldipeas@disroot.org>
2022-10-14 17:06:10 -0300

    ^linux-tkg-build +linux-tkg-install

--- linux-tkg-build.sh
+++ linux-tkg-build.sh
@@ -3,4 +3,5 @@ set -e
 git clone https://github.com/Frogging-Family/linux-tkg
 cd linux-tkg
 cp ../linux-tkg-customization.cfg customization.cfg
-yes|./install.sh install
+cp ../linux-tkg-install.sh install.sh
+./install.sh install
