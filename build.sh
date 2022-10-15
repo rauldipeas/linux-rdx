be8798bf269c927090a6ed4b279995bfd0bee0e1
Raul Dipeas <rauldipeas@disroot.org>
2022-10-15 12:27:13 -0300

    ^build

--- build.sh
+++ build.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -e
-sudo apt install\
-    libelf-dev
+sudo apt install libelf-dev #bison build-essential flex libncurses-dev libssl-dev
 wget -c https://cdn.kernel.org/pub/linux/kernel/v6.x/$(wget -qO- https://cdn.kernel.org/pub/linux/kernel/v6.x/|grep tar.xz|cut -d '"' -f2|tail -n2|head -n1)
 tar xavf $(wget -qO- https://cdn.kernel.org/pub/linux/kernel/v6.x/|grep tar.xz|cut -d '"' -f2|tail -n2|head -n1)
 cd ./linux-$(wget -qO- https://cdn.kernel.org/pub/linux/kernel/v6.x/|grep tar.xz|cut -d '"' -f2|tail -n2|head -n1|sed 's/linux-//g'|sed 's/.tar.xz//g')
