1210c0f7da8d5d4086b7c3b5fc59c93ecdad77d4
Raul Dipeas <rauldipeas@disroot.org>
2022-10-15 10:47:45 -0300

    ^build

--- build.sh
+++ build.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 set -e
 sudo apt install\
-    bison\
-    build-essential\
-    flex\
-    libncurses-dev\
-    libssl-dev
+    libelf-dev
 wget -c https://cdn.kernel.org/pub/linux/kernel/v6.x/$(wget -qO- https://cdn.kernel.org/pub/linux/kernel/v6.x/|grep tar.xz|cut -d '"' -f2|tail -n2|head -n1)
 tar xavf $(wget -qO- https://cdn.kernel.org/pub/linux/kernel/v6.x/|grep tar.xz|cut -d '"' -f2|tail -n2|head -n1)
 cd ./linux-$(wget -qO- https://cdn.kernel.org/pub/linux/kernel/v6.x/|grep tar.xz|cut -d '"' -f2|tail -n2|head -n1|sed 's/linux-//g'|sed 's/.tar.xz//g')
