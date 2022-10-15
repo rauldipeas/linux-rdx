238f92c93dfa19f4fd3b809823ae692930f8ce27
Raul Dipeas <rauldipeas@disroot.org>
2022-10-15 10:39:53 -0300

    ^build

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ sudo apt install\
      libssl-dev
 wget -c https://cdn.kernel.org/pub/linux/kernel/v6.x/$(wget -qO- https://cdn.kernel.org/pub/linux/kernel/v6.x/|grep tar.xz|cut -d '"' -f2|tail -n2|head -n1)
 tar xavf $(wget -qO- https://cdn.kernel.org/pub/linux/kernel/v6.x/|grep tar.xz|cut -d '"' -f2|tail -n2|head -n1)
-cd ./linux-*
+cd ./linux-$(wget -qO- https://cdn.kernel.org/pub/linux/kernel/v6.x/|grep tar.xz|cut -d '"' -f2|tail -n2|head -n1|sed 's/linux-//g'|sed 's/.tar.xz//g')
 cp ../kernel-config .config
 make olddefconfig
 make clean
