fcb485b89fc71f3b7b0bef42db8c47fd86362050
Raul Dipeas <rauldipeas@disroot.org>
2022-10-15 14:14:14 -0300

    ^build ^linux-rauldipeas

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ wget -c https://cdn.kernel.org/pub/linux/kernel/v6.x/$(wget -qO- https://cdn.ker
 tar xavf $(wget -qO- https://cdn.kernel.org/pub/linux/kernel/v6.x/|grep tar.xz|cut -d '"' -f2|tail -n2|head -n1)
 cd ./linux-$(wget -qO- https://cdn.kernel.org/pub/linux/kernel/v6.x/|grep tar.xz|cut -d '"' -f2|tail -n2|head -n1|sed 's/linux-//g'|sed 's/.tar.xz//g')
 cp ../kernel-config .config
-make olddefconfig
-make clean
-make -j `getconf _NPROCESSORS_ONLN`
-make deb-pkg LOCALVERSION=-rauldipeas
+make CC=clang olddefconfig
+make CC=clang clean
+make CC=clang -j `getconf _NPROCESSORS_ONLN`
+make CC=clang deb-pkg LOCALVERSION=-rauldipeas
