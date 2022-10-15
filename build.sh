4b151b4b2d02d145538809101053e55abaa2eb39
Raul Dipeas <rauldipeas@disroot.org>
2022-10-15 10:33:18 -0300

    +linux-rauldipeas

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+sudo apt install\
+     bison\
+     flex\
+     libncurses-dev\
+     libssl-dev
+wget -c https://cdn.kernel.org/pub/linux/kernel/v6.x/$(wget -qO- https://cdn.kernel.org/pub/linux/kernel/v6.x/|grep tar.xz|cut -d '"' -f2|tail -n2|head -n1)
+tar xavf $(wget -qO- https://cdn.kernel.org/pub/linux/kernel/v6.x/|grep tar.xz|cut -d '"' -f2|tail -n2|head -n1)
+cd ./linux-*
+cp ../kernel-config .config
+make olddefconfig
+make clean
+make -j `getconf _NPROCESSORS_ONLN`
+make deb-pkg LOCALVERSION=-rauldipeas
--- linux-tkg-build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-git clone https://github.com/Frogging-Family/linux-tkg
-cd linux-tkg
-cp ../linux-tkg-customization.cfg customization.cfg
-cp ../linux-tkg-install.sh install.sh
-./install.sh install
