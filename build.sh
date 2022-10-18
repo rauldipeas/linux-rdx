9db0f16e2eee490109d30ab8c9dfdf9342056ac5
Raul Dipeas <rauldipeas@disroot.org>
2022-10-18 18:57:31 -0300

    ^build

--- build.sh
+++ build.sh
@@ -7,5 +7,5 @@ tar xavf $(wget -qO- https://cdn.kernel.org/pub/linux/kernel/v6.x/|grep tar.xz|c
 cd ./linux-$(wget -qO- https://cdn.kernel.org/pub/linux/kernel/v6.x/|grep tar.xz|cut -d '"' -f2|tail -n2|head -n1|sed 's/linux-//g'|sed 's/.tar.xz//g')
 cp ../kernel-config .config
 cp -rfv ../revertions/kernel-tree/* .
-make CC=clang olddefconfig
-make CC=clang -j `getconf _NPROCESSORS_ONLN` deb-pkg LOCALVERSION=-rdx
+make olddefconfig
+make -j `getconf _NPROCESSORS_ONLN` deb-pkg LOCALVERSION=-rdx
