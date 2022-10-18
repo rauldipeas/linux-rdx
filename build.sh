7ba0ed577999a8d30252ce81267ccf97f8a3ada6
Raul Dipeas <rauldipeas@disroot.org>
2022-10-18 06:53:08 -0300

    ^build

--- build.sh
+++ build.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 set -e
-sudo apt install libelf-dev #bison build-essential flex libncurses-dev libssl-dev
+sudo apt install libelf-dev liblz4-tool #bison build-essential flex libncurses-dev libssl-dev
+#bison flex libfl-dev libfl2 liblz4-tool libncurses-dev libssl-dev lz4 m4
 wget -c https://cdn.kernel.org/pub/linux/kernel/v6.x/$(wget -qO- https://cdn.kernel.org/pub/linux/kernel/v6.x/|grep tar.xz|cut -d '"' -f2|tail -n2|head -n1)
 tar xavf $(wget -qO- https://cdn.kernel.org/pub/linux/kernel/v6.x/|grep tar.xz|cut -d '"' -f2|tail -n2|head -n1)
 cd ./linux-$(wget -qO- https://cdn.kernel.org/pub/linux/kernel/v6.x/|grep tar.xz|cut -d '"' -f2|tail -n2|head -n1|sed 's/linux-//g'|sed 's/.tar.xz//g')
-#cp ../kernel-config .config
-#make olddefconfig
-make defconfig
+cp ../kernel-config .config
+make olddefconfig
+#make defconfig
 make clean
-make -j `getconf _NPROCESSORS_ONLN`
+make V=1 -j `getconf _NPROCESSORS_ONLN`
 make deb-pkg LOCALVERSION=-rdx
