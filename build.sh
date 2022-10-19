ef40ae2993eeddad9dc8fcde8f03fb182eae0842
Raul Dipeas <rauldipeas@disroot.org>
2022-10-18 23:54:43 -0300

    ^build

--- build.sh
+++ build.sh
@@ -5,12 +5,12 @@ sudo apt install libelf-dev liblz4-tool #bison build-essential flex libncurses-d
 wget -c https://cdn.kernel.org/pub/linux/kernel/v6.x/$(wget -qO- https://cdn.kernel.org/pub/linux/kernel/v6.x/|grep tar.xz|cut -d '"' -f2|tail -n2|head -n1)
 tar xavf $(wget -qO- https://cdn.kernel.org/pub/linux/kernel/v6.x/|grep tar.xz|cut -d '"' -f2|tail -n2|head -n1)
 cd ./linux-$(wget -qO- https://cdn.kernel.org/pub/linux/kernel/v6.x/|grep tar.xz|cut -d '"' -f2|tail -n2|head -n1|sed 's/linux-//g'|sed 's/.tar.xz//g')
-cp ../kernel-config .config
+#cp ../kernel-config .config
 rm -rfv drivers/gpu/drm/amd/pm/powerplay/hwmqr/smu7_hwmgr.c
 rm -rfv drivers/gpu/drm/amd/pm/powerplay/hwmqr/vega10_hwmgr.c
 rm -rfv drivers/gpu/drm/amd/pm/powerplay/hwmqr/vega12_hwmgr.c
 rm -rfv drivers/gpu/drm/amd/pm/powerplay/hwmqr/vega20_hwmgr.c
 rm -rfv mm/z3fold.c
 rm -rfv security/integrity/iintc.c
-make olddefconfig
+make defconfig
 make CC=clang -j `getconf _NPROCESSORS_ONLN` deb-pkg LOCALVERSION=-rdx
