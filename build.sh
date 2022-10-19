3ba57eacc38be5b9a6a4d6a485d67b58494de16e
Raul Dipeas <rauldipeas@disroot.org>
2022-10-19 09:18:25 -0300

    ^build

--- build.sh
+++ build.sh
@@ -6,6 +6,8 @@ wget -c https://cdn.kernel.org/pub/linux/kernel/v6.x/$(wget -qO- https://cdn.ker
 tar xavf $(wget -qO- https://cdn.kernel.org/pub/linux/kernel/v6.x/|grep tar.xz|cut -d '"' -f2|tail -n2|head -n1)
 cd ./linux-$(wget -qO- https://cdn.kernel.org/pub/linux/kernel/v6.x/|grep tar.xz|cut -d '"' -f2|tail -n2|head -n1|sed 's/linux-//g'|sed 's/.tar.xz//g')
 patch -p1<../99-m-audio_fast_track_c400-c600.patch
-make defconfig
+wget -O .config https://raw.githubusercontent.com/archlinux/svntogit-packages/packages/linux/trunk/config
+sed -i 's/CONFIG_DEFAULT_HOSTNAME/#CONFIG_DEFAULT_HOSTNAME/g' .config
+make olddefconfig
 scripts/config --disable DEBUG_INFO
 make -j `getconf _NPROCESSORS_ONLN` deb-pkg LOCALVERSION=-rdx
