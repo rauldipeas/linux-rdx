a4d51e0978058ad7c293b54e58f9c0dd041e360b
Raul Dipeas <rauldipeas@disroot.org>
2022-10-19 08:15:23 -0300

    +99-m-audio_fast_track_c400-c600 ^build

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ sudo apt install libelf-dev liblz4-tool #bison build-essential flex libncurses-d
 wget -c https://cdn.kernel.org/pub/linux/kernel/v6.x/$(wget -qO- https://cdn.kernel.org/pub/linux/kernel/v6.x/|grep tar.xz|cut -d '"' -f2|tail -n2|head -n1)
 tar xavf $(wget -qO- https://cdn.kernel.org/pub/linux/kernel/v6.x/|grep tar.xz|cut -d '"' -f2|tail -n2|head -n1)
 cd ./linux-$(wget -qO- https://cdn.kernel.org/pub/linux/kernel/v6.x/|grep tar.xz|cut -d '"' -f2|tail -n2|head -n1|sed 's/linux-//g'|sed 's/.tar.xz//g')
-cp ../sound-usb-implicit-c400-fix.c sound/usb/implicit.c
-cp ../kernel-config .config
+patch -p1<../99-m-audio_fast_track_c400-c600.patch
+#cp ../kernel-config .config
 make olddefconfig
 make -j `getconf _NPROCESSORS_ONLN` deb-pkg LOCALVERSION=-rdx
