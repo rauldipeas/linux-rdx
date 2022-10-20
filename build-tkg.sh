5c0c43de68fa862fccdff325a88a76cff8065950
Raul Dipeas <rauldipeas@disroot.org>
2022-10-20 11:58:09 -0300

    ^build ^linux-rdx ^readme

--- build-tkg.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-git clone https://github.com/Frogging-Family/linux-tkg
-cd linux-tkg
-cp ../customization.cfg .
-cp ../install.sh .
-mkdir linux60-tkg-userpatches
-cp ../99-m-audio_fast_track_c400-c600.patch linux60-tkg-userpatches/99-m-audio_fast_track_c400-c600.mypatch
-./install.sh install
--- build.sh
+++ build.sh
@@ -6,10 +6,8 @@ wget -c https://cdn.kernel.org/pub/linux/kernel/v6.x/$(wget -qO- https://cdn.ker
 tar xaf $(wget -qO- https://cdn.kernel.org/pub/linux/kernel/v6.x/|grep tar.xz|cut -d '"' -f2|tail -n2|head -n1)
 cd ./linux-$(wget -qO- https://cdn.kernel.org/pub/linux/kernel/v6.x/|grep tar.xz|cut -d '"' -f2|tail -n2|head -n1|sed 's/linux-//g'|sed 's/.tar.xz//g')
 patch -p1<../99-m-audio_fast_track_c400-c600.patch
-#wget -O .config https://kernel.ubuntu.com/~kernel-ppa/config/jammy/linux/$(wget -qO- https://kernel.ubuntu.com/~kernel-ppa/config/jammy/linux/|grep href|cut -d '"' -f8|tail -n1)amd64-config.flavour.lowlatency
 wget -O .config https://raw.githubusercontent.com/Frogging-Family/linux-tkg/master/linux-tkg-config/5.10/config.x86_64
 make olddefconfig
 sed -i 's/CONFIG_DEBUG_INFO_BTF=y/CONFIG_DEBUG_INFO_BTF=n/g' .config
 sed -i 's/CONFIG_X509_CERTIFICATE_PARSER=y/CONFIG_X509_CERTIFICATE_PARSER=n/g' .config
-#cp ../config .config
 make -j `getconf _NPROCESSORS_ONLN` deb-pkg LOCALVERSION=-rdx
