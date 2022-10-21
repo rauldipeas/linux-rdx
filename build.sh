64ec4b8887ff350ca40aa4c229efe05309df10a1
Raul Dipeas <rauldipeas@disroot.org>
2022-10-21 11:22:12 -0300

    ^build

--- build.sh
+++ build.sh
@@ -8,7 +8,8 @@ tar xaf $(wget -qO- https://cdn.kernel.org/pub/linux/kernel/v6.x/|grep tar.xz|cu
 rm $(wget -qO- https://cdn.kernel.org/pub/linux/kernel/v6.x/|grep tar.xz|cut -d '"' -f2|tail -n2|head -n1)
 cd linux*
 patch -p1<../patches/99-m-audio_fast_track_c400-c600.patch
-cp ../config/config-xanmod-6.0.2-edge .config
+#cp ../config/xanmod-edge-6.0.2-x86-64-v2 .config
+wget -qO .config https://raw.githubusercontent.com/xanmod/linux/6.0/CONFIGS/xanmod/gcc/config_x86-64-v2
 make olddefconfig
 scripts/config --disable CONFIG_DEBUG_INFO
 make -j `getconf _NPROCESSORS_ONLN` deb-pkg LOCALVERSION=-rdx
