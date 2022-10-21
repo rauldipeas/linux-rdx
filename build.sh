637bfdccdb89e78286f1540c43534ebd356c15d6
Raul Dipeas <rauldipeas@disroot.org>
2022-10-21 10:50:55 -0300

    ^build

--- build.sh
+++ build.sh
@@ -2,10 +2,13 @@
 set -e
 sudo apt install libelf-dev liblz4-tool #bison build-essential flex libncurses-dev libssl-dev
 #bison flex libfl-dev libfl2 liblz4-tool libncurses-dev libssl-dev lz4 m4
-git clone https://github.com/xanmod/linux
-cd linux
+#git clone https://github.com/xanmod/linux
+wget -qc https://cdn.kernel.org/pub/linux/kernel/v6.x/$(wget -qO- https://cdn.kernel.org/pub/linux/kernel/v6.x/|grep tar.xz|cut -d '"' -f2|tail -n2|head -n1)
+tar xaf $(wget -qO- https://cdn.kernel.org/pub/linux/kernel/v6.x/|grep tar.xz|cut -d '"' -f2|tail -n2|head -n1)
+rm $(wget -qO- https://cdn.kernel.org/pub/linux/kernel/v6.x/|grep tar.xz|cut -d '"' -f2|tail -n2|head -n1)
+cd linux*
 patch -p1<../patches/99-m-audio_fast_track_c400-c600.patch
-cp ../config/config-6.0.2-edge .config
+cp ../config/config-xanmod-6.0.2-edge .config
 make olddefconfig
 scripts/config --disable CONFIG_DEBUG_INFO
 make -j `getconf _NPROCESSORS_ONLN` deb-pkg LOCALVERSION=-rdx
