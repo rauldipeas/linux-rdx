1405e47064093382e422ba37c0e5e74105b8c08d
Raul Dipeas <rauldipeas@disroot.org>
2022-11-09 12:52:41 +0000

    ^scripts

--- scripts/config-file.sh
+++ scripts/config-file.sh
@@ -5,7 +5,6 @@ set -e
 cd linux*
 wget -qO .config https://raw.githubusercontent.com/xanmod/linux/6.0/CONFIGS/xanmod/gcc/config_x86-64-v3
 sed -i 's/x64v3/rdx/g' .config
-make olddefconfig
 scripts/config --set-val CONFIG_DEBUG_INFO 'n'
 scripts/config --set-val CONFIG_IRQ_FORCED_THREADING 'y'
 scripts/config --set-val CONFIG_LOGLEVEL_QUIET '0'
@@ -13,4 +12,5 @@ scripts/config --set-val CONFIG_PREEMPT 'y'
 scripts/config --set-val CONFIG_RETPOLINE 'n'
 scripts/config --set-val CONFIG_SLS 'n'
 scripts/config --set-val CONFIG_SPECULATION_MITIGATIONS 'n'
+make olddefconfig
 cat .config
