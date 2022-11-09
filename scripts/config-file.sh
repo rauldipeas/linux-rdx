b7fbde7fec6922499ab9e6bf3a0bd7a724b53a6e
Raul Dipeas <rauldipeas@disroot.org>
2022-11-09 03:34:05 +0000

    ^scripts

--- scripts/config-file.sh
+++ scripts/config-file.sh
@@ -4,10 +4,10 @@ set -e
 # Config file
 cd linux*
 wget -qO .config https://raw.githubusercontent.com/xanmod/linux/6.0/CONFIGS/xanmod/gcc/config_x86-64-v3
+sed -i 's/x64v3/rdx/g' .config
 make olddefconfig
 scripts/config --disable CONFIG_DEBUG_INFO
 scripts/config --enable CONFIG_IRQ_FORCED_THREADING
-scripts/config --set-val CONFIG_LOCALVERSION '-rdx'
 scripts/config --set-val CONFIG_LOGLEVEL_QUIET 0
 scripts/config --enable CONFIG_PREEMPT
 scripts/config --disable CONFIG_RETPOLINE
