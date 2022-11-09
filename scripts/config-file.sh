07d95c29250dbd3eaceb6d31a5daf3823a8499dc
Raul Dipeas <rauldipeas@disroot.org>
2022-11-09 22:50:00 +0000

    ^scripts

--- scripts/config-file.sh
+++ scripts/config-file.sh
@@ -7,10 +7,10 @@ wget -qO .config https://raw.githubusercontent.com/xanmod/linux/6.0/CONFIGS/xanm
 sed -i 's/x64v1/rdx/g' .config
 make olddefconfig
 scripts/config --disable CONFIG_DEBUG_INFO
-scripts/config --enable CONFIG_IRQ_FORCE_THREADING #sed -i 's/CONFIG_IRQ_FORCED_THREADING=y/CONFIG_IRQ_FORCED_THREADING=y/g' .config
-scripts/config --set-val CONFIG_LOGLEVEL_QUIET '0' #sed -i 's/CONFIG_LOGLEVEL_QUIET=3/CONFIG_CONSOLE_LOGLEVEL_QUIET=0/g' .config
-scripts/config --enable CONFIG_PREEMPT #sed -i 's/# CONFIG_PREEMPT is not set/CONFIG_PREEMPT=y/g' .config
-scripts/config --set-val CONFIG_RETPOLINE 'n' #sed -i 's/CONFIG_RETPOLINE=y/CONFIG_RETPOLINE=n/g' .config
-scripts/config --set-val CONFIG_SLS 'n' #sed -i 's/CONFIG_SLS=y/CONFIG_SLS=n/g' .config
-scripts/config --set-val CONFIG_SPECULATION_MITIGATIONS 'n' #sed -i 's/CONFIG_SPECULATION_MITIGATIONS=y/CONFIG_SPECULATION_MITIGATIONS=n/g' .config
+#scripts/config --enable CONFIG_IRQ_FORCE_THREADING #sed -i 's/CONFIG_IRQ_FORCED_THREADING=y/CONFIG_IRQ_FORCED_THREADING=y/g' .config
+#scripts/config --set-val CONFIG_LOGLEVEL_QUIET '0' #sed -i 's/CONFIG_LOGLEVEL_QUIET=3/CONFIG_CONSOLE_LOGLEVEL_QUIET=0/g' .config
+#scripts/config --enable CONFIG_PREEMPT #sed -i 's/# CONFIG_PREEMPT is not set/CONFIG_PREEMPT=y/g' .config
+#scripts/config --set-val CONFIG_RETPOLINE 'n' #sed -i 's/CONFIG_RETPOLINE=y/CONFIG_RETPOLINE=n/g' .config
+#scripts/config --set-val CONFIG_SLS 'n' #sed -i 's/CONFIG_SLS=y/CONFIG_SLS=n/g' .config
+#scripts/config --set-val CONFIG_SPECULATION_MITIGATIONS 'n' #sed -i 's/CONFIG_SPECULATION_MITIGATIONS=y/CONFIG_SPECULATION_MITIGATIONS=n/g' .config
 cat .config
