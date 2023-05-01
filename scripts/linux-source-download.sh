271814ea97e2c96e80caad3e4bc67a8000ff8c90
Raul Dipeas <rdipeas+github@proton.me>
2023-05-01 00:47:07 -0300

    ^scripts

--- scripts/linux-source-download.sh
+++ scripts/linux-source-download.sh
@@ -2,9 +2,9 @@
 set -e
 
 # Linux source download
-#wget -qc --show-progress $(wget -qO- https://kernel.org/|grep tar.xz|cut -d '"' -f2|head -n1)
-#tar xaf $(wget -qO- https://kernel.org/|grep tar.xz|cut -d '"' -f2|head -n1|cut -d '/' -f8)
-#rm $(wget -qO- https://kernel.org/|grep tar.xz|cut -d '"' -f2|head -n1|cut -d '/' -f8)
-git clone git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git linux-git
-cd linux*
-git checkout v"$(wget -qO- https://kernel.org/|grep tar.xz|cut -d '"' -f2|head -n1|cut -d '/' -f8|cut -d '-' -f2|sed 's/.tar.xz//g')"
+wget -qc --show-progress $(wget -qO- https://kernel.org/|grep tar.xz|cut -d '"' -f2|head -n1)
+tar xaf $(wget -qO- https://kernel.org/|grep tar.xz|cut -d '"' -f2|head -n1|cut -d '/' -f8)
+rm $(wget -qO- https://kernel.org/|grep tar.xz|cut -d '"' -f2|head -n1|cut -d '/' -f8)
+#git clone git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git linux-git
+#cd linux*
+#git checkout v"$(wget -qO- https://kernel.org/|grep tar.xz|cut -d '"' -f2|head -n1|cut -d '/' -f8|cut -d '-' -f2|sed 's/.tar.xz//g')"
