68bae9977a9e3c5ba295afba07f8d4ac595ac7e5
Raul Dipeas <rdipeas+github@proton.me>
2023-05-01 01:02:31 -0300

    ^scripts

--- scripts/linux-source-download.sh
+++ scripts/linux-source-download.sh
@@ -5,6 +5,8 @@ set -e
 wget -qc --show-progress $(wget -qO- https://kernel.org/|grep tar.xz|cut -d '"' -f2|head -n1)
 tar xaf $(wget -qO- https://kernel.org/|grep tar.xz|cut -d '"' -f2|head -n1|cut -d '/' -f8)
 rm $(wget -qO- https://kernel.org/|grep tar.xz|cut -d '"' -f2|head -n1|cut -d '/' -f8)
+cd linux*
+git init
 #git clone git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git linux-git
 #cd linux*
 #git checkout v"$(wget -qO- https://kernel.org/|grep tar.xz|cut -d '"' -f2|head -n1|cut -d '/' -f8|cut -d '-' -f2|sed 's/.tar.xz//g')"
