b0431895341648f9382b9a748e2976b3ad7e4636
Raul Dipeas <rdipeas+github@proton.me>
2023-06-17 18:09:29 -0300

    ^scripts

--- scripts/actions-cleanup.sh
+++ scripts/actions-cleanup.sh
@@ -3,7 +3,7 @@ set -e
 
 # Actions cleanup
 PROJECT='rauldipeas/linux-rdx'
-KEEP=2
+KEEP=1
 gh api\
 	repos/"$PROJECT"/actions/runs\
 	--paginate -q '.workflow_runs[]|select(.head_branch != "any")|"\(.id)"'|tail -n+$((KEEP+1))\
