5957ac2f21fc74156c71bfdb8f07fc484ac3077c
Raul Dipeas <rauldipeas@disroot.org>
2024-08-26 01:00:28 -0300

    *update to v4 actions

--- scripts/actions-cleanup.sh
+++ scripts/actions-cleanup.sh
@@ -3,8 +3,5 @@ set -e
 
 # Actions cleanup
 PROJECT='rauldipeas/linux-rdx'
-KEEP=1
-gh api\
-	repos/"$PROJECT"/actions/runs\
-	--paginate -q '.workflow_runs[]|select(.head_branch != "any")|"\(.id)"'|tail -n+$((KEEP+1))\
-    |xargs -n1 -I % gh api repos/"$PROJECT"/actions/runs/% -X DELETE
+KEEP=3
+gh api repos/"$PROJECT"/actions/runs --paginate -q '.workflow_runs[]|select(.head_branch != "any")|"\(.id)"'|tail -n+$((KEEP+1))|xargs -n1 -I % gh api repos/"$PROJECT"/actions/runs/% -X DELETE
--- scripts/linux-source-download.sh
+++ scripts/linux-source-download.sh
@@ -9,6 +9,6 @@ set -e
 #git checkout v"$(wget -qO- https://kernel.org/|grep tar.xz|cut -d '"' -f2|head -n1|cut -d '/' -f8|cut -d '-' -f2|sed 's/.tar.xz//g'|cut -d '.' -f1-2)"
 
 ## Tarball
-wget -qc --show-progress $(wget -qO- https://kernel.org/|grep tar.xz|cut -d '"' -f2|head -n1)
-tar xaf $(wget -qO- https://kernel.org/|grep tar.xz|cut -d '"' -f2|head -n1|cut -d '/' -f8)
-rm $(wget -qO- https://kernel.org/|grep tar.xz|cut -d '"' -f2|head -n1|cut -d '/' -f8)
+wget -qc --show-progress "$(wget -qO- https://kernel.org/|grep tar.xz|cut -d '"' -f2|head -n1)"
+tar xaf "$(wget -qO- https://kernel.org/|grep tar.xz|cut -d '"' -f2|head -n1|cut -d '/' -f8)"
+rm "$(wget -qO- https://kernel.org/|grep tar.xz|cut -d '"' -f2|head -n1|cut -d '/' -f8)"
