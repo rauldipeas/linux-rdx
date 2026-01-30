cac69fdc35deeb7d7e49a23921c672e5ac595096
Raul Dipeas <rauldipeas@disroot.org>
2026-01-30 00:21:02 -0300

    ^config-file

--- scripts/config-file.sh
+++ scripts/config-file.sh
@@ -17,35 +17,35 @@ sed -i 's/^EXTRAVERSION *=.*/EXTRAVERSION = -rdx/' Makefile
 
 # Misc settings
 ## LOGLEVEL e PREEMPT
-sed -i 's/^CONFIG_LOGLEVEL_QUIET=.*/CONFIG_LOGLEVEL_QUIET=0/' .config
-sed -i 's/^# CONFIG_LOGLEVEL_QUIET is not set/CONFIG_LOGLEVEL_QUIET=0/' .config
-sed -i 's/^CONFIG_PREEMPT=.*/CONFIG_PREEMPT=y/' .config
-sed -i 's/^# CONFIG_PREEMPT is not set/CONFIG_PREEMPT=y/' .config
+#sed -i 's/^CONFIG_LOGLEVEL_QUIET=.*/CONFIG_LOGLEVEL_QUIET=0/' .config
+#sed -i 's/^# CONFIG_LOGLEVEL_QUIET is not set/CONFIG_LOGLEVEL_QUIET=0/' .config
+#sed -i 's/^CONFIG_PREEMPT=.*/CONFIG_PREEMPT=y/' .config
+#sed -i 's/^# CONFIG_PREEMPT is not set/CONFIG_PREEMPT=y/' .config
 ## Desativa mitigations
-sed -i '/^CONFIG_RETPOLINE=/d' .config
-sed -i '/^# CONFIG_RETPOLINE is not set/d' .config
-echo "# CONFIG_RETPOLINE is not set" >> .config
-sed -i '/^CONFIG_SLS=/d' .config
-sed -i '/^# CONFIG_SLS is not set/d' .config
-echo "# CONFIG_SLS is not set" >> .config
-sed -i '/^CONFIG_SPECULATION_MITIGATIONS=/d' .config
-sed -i '/^# CONFIG_SPECULATION_MITIGATIONS is not set/d' .config
-echo "# CONFIG_SPECULATION_MITIGATIONS is not set" >> .config
+#sed -i '/^CONFIG_RETPOLINE=/d' .config
+#sed -i '/^# CONFIG_RETPOLINE is not set/d' .config
+#echo "# CONFIG_RETPOLINE is not set" >> .config
+#sed -i '/^CONFIG_SLS=/d' .config
+#sed -i '/^# CONFIG_SLS is not set/d' .config
+#echo "# CONFIG_SLS is not set" >> .config
+#sed -i '/^CONFIG_SPECULATION_MITIGATIONS=/d' .config
+#sed -i '/^# CONFIG_SPECULATION_MITIGATIONS is not set/d' .config
+#echo "# CONFIG_SPECULATION_MITIGATIONS is not set" >> .config
 ## Ativa DEBUG_INFO_NONE
-sed -i '/^CONFIG_DEBUG_INFO_NONE=/d' .config
-sed -i '/^# CONFIG_DEBUG_INFO_NONE is not set/d' .config
-echo "CONFIG_DEBUG_INFO_NONE=y" >> .config
+#sed -i '/^CONFIG_DEBUG_INFO_NONE=/d' .config
+#sed -i '/^# CONFIG_DEBUG_INFO_NONE is not set/d' .config
+#echo "CONFIG_DEBUG_INFO_NONE=y" >> .config
 ## Desativa chaves de confiança do sistema
-for opt in CONFIG_SYSTEM_REVOCATION_KEYS CONFIG_SYSTEM_TRUSTED_KEYS; do
-  sed -i "/^$opt=/d" .config
-  sed -i "/^# $opt is not set/d" .config
-  echo "# $opt is not set" >> .config
-done
+#for opt in CONFIG_SYSTEM_REVOCATION_KEYS CONFIG_SYSTEM_TRUSTED_KEYS; do
+#  sed -i "/^$opt=/d" .config
+#  sed -i "/^# $opt is not set/d" .config
+#  echo "# $opt is not set" >> .config
+#done
 
 # rtcqs settings
-sed -i 's/^CONFIG_LOGLEVEL_QUIET=.*/CONFIG_LOGLEVEL_QUIET=0/' .config
-sed -i 's/^# CONFIG_LOGLEVEL_QUIET is not set/CONFIG_LOGLEVEL_QUIET=0/' .config
-sed -i 's/^CONFIG_PREEMPT=.*/CONFIG_PREEMPT=y/' .config
-sed -i 's/^# CONFIG_PREEMPT is not set/CONFIG_PREEMPT=y/' .config
+#sed -i 's/^CONFIG_LOGLEVEL_QUIET=.*/CONFIG_LOGLEVEL_QUIET=0/' .config
+#sed -i 's/^# CONFIG_LOGLEVEL_QUIET is not set/CONFIG_LOGLEVEL_QUIET=0/' .config
+#sed -i 's/^CONFIG_PREEMPT=.*/CONFIG_PREEMPT=y/' .config
+#sed -i 's/^# CONFIG_PREEMPT is not set/CONFIG_PREEMPT=y/' .config
 
 make olddefconfig
