d8303634ea4e7b1d27f0fb81daa075e8999a31dd
Raul Dipeas <rauldipeas@disroot.org>
2026-01-31 13:40:17 -0300

    ^build ^linux-rdx ^upload

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,3 +5,4 @@ bash -x scripts/linux-source-download.sh
 bash -x scripts/patches.sh
 bash -x scripts/config-file.sh
 bash -x scripts/bindeb-pkg.sh
+bash -x scripts/upload.sh
---
+++ scripts/upload.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -e
+# Linux RDX
+wget -q --show-progress https://nightly.link/rauldipeas/linux-rdx/workflows/linux-rdx/main/linux-rdx.zip
+unzip -oqq linux-rdx.zip
+rm linux-libc-dev_*_amd64.deb linux-rdx.zip
+dpkg-deb -x linux-image-*-rdx_*_amd64.deb repack-image
+dpkg-deb -e linux-image-*-rdx_*_amd64.deb repack-image/DEBIAN
+rm linux-image-*-rdx_*_amd64.deb
+sed -i "s/linux-image-.*/linux-image-rdx/g" repack-image/DEBIAN/control
+dpkg-deb -b --root-owner-group repack-image .
+dpkg-deb -x linux-headers-*-rdx_*_amd64.deb repack-headers
+dpkg-deb -e linux-headers-*-rdx_*_amd64.deb repack-headers/DEBIAN
+rm linux-headers-*-rdx_*_amd64.deb
+sed -i "s/linux-headers-.*/linux-headers-rdx/g" repack-headers/DEBIAN/control
+dpkg-deb -b --root-owner-group repack-headers .
+mkdir -p meta-rdx/DEBIAN
+META_VERSION="$(find ./*rdx*.deb | head -n1 | cut -d "_" -f2 | cut -d "-" -f1)"
+BUILD='1'
+cat <<EOF | tee meta-rdx/DEBIAN/control >/dev/null
+Package: linux-rdx
+Version: $META_VERSION-rdx-$BUILD
+Architecture: amd64
+Maintainer: Raul Dipeas <rauldipeas@disroot.org>
+Depends: linux-image-rdx (= $META_VERSION-rdx-$BUILD), linux-headers-rdx (= $META_VERSION-rdx-$BUILD)
+Section: kernel
+Priority: optional
+Description: Este pacote contém uma versão com pequenas modificações do kernel Linux, com alguns patches de correções para o meu hardware pessoal.
+EOF
+dpkg-deb -b --root-owner-group meta-rdx .
+rm -r meta-rdx repack-*
+for PACKAGE in *.deb; do
+	curl -sF package=@"$PACKAGE" https://"$GEMFURY_PUSH_TOKEN"@push.fury.io/linux-rdx/
+done
