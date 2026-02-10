13596c4b043d36d51a49b1ae868309223ebbf4b4
Raul Dipeas <github@rauldipeas.com.br>
2026-02-09 21:30:07 -0300

    ^linux-source-download

--- scripts/linux-source-download.sh
+++ scripts/linux-source-download.sh
@@ -2,14 +2,8 @@
 set -e
 
 # Linux source download
-
-## Git
-#git clone git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git linux-git
-#cd linux*
-#git checkout v"$(wget -qO- https://kernel.org/|grep tar.xz|cut -d '"' -f2|head -n1|cut -d '/' -f8|cut -d '-' -f2|sed 's/.tar.xz//g'|cut -d '.' -f1-2)"
-
 ## Tarball
-#wget -q --show-progress "$(wget -qO- https://kernel.org/|grep tar.xz|cut -d '"' -f2|head -n1)"
-wget -q --show-progress https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.18.9.tar.xz
-tar xaf "$(wget -qO- https://kernel.org/|grep tar.xz|cut -d '"' -f2|head -n1|cut -d '/' -f8)"
-rm "$(wget -qO- https://kernel.org/|grep tar.xz|cut -d '"' -f2|head -n1|cut -d '/' -f8)"
+LATEST_KERNEL="$(curl -s https://www.kernel.org/ | grep -A1 'stable:' | grep -oP '\d+\.\d+\.\d+' | head -1)"
+wget https://cdn.kernel.org/pub/linux/kernel/v"${LATEST_KERNEL%%.*}".x/linux-"${LATEST_KERNEL}".tar.xz
+tar xaf linux-"${LATEST_KERNEL}".tar.xz
+rm linux-"${LATEST_KERNEL}".tar.xz
