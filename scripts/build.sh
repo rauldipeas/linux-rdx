bae6792d45674c2d8c6f98bdc2bfb800a1c48292
Raul Dipeas <github@rauldipeas.com.br>
2026-02-17 00:50:36 -0300

    ^build ^linux-rdx ^nvidia-check

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,4 +5,5 @@ bash -x scripts/linux-source-download.sh
 bash -x scripts/patches.sh
 bash -x scripts/config-file.sh
 bash -x scripts/bindeb-pkg.sh
+bash -x scripts/nvidia-check.sh
 bash -x scripts/upload.sh
--- scripts/nvidia-check.sh
+++ scripts/nvidia-check.sh
@@ -1,26 +1,24 @@
 #!/bin/bash
 set -e
-
-# Pega a versão mais recente do kernel
-LATEST_KERNEL="$(curl -s https://www.kernel.org/finger_banner | grep 'latest stable version' | awk '{print $NF}')"
-KERNEL_MAJOR="$(echo "$LATEST_KERNEL" | cut -d. -f1)"
-echo "🔍 Baixando kernel ${LATEST_KERNEL}..."
-wget -q --show-progress https://cdn.kernel.org/pub/linux/kernel/v"${KERNEL_MAJOR}".x/linux-"${LATEST_KERNEL}".tar.xz
-tar xf linux-"${LATEST_KERNEL}".tar.xz
-cd linux-"${LATEST_KERNEL}"
-KERNEL_SRC_DIR="$(pwd)"
-KERNEL_VERSION="$(make -s kernelversion)"
-echo "🔍 Baixando driver NVIDIA..."
-LATEST_VERSION="$(curl -s "https://download.nvidia.com/XFree86/Linux-x86_64/latest.txt")"
-DOWNLOAD_URL="https://download.nvidia.com/XFree86/Linux-x86_64/${LATEST_VERSION}/NVIDIA-Linux-x86_64-${LATEST_VERSION}.run"
-wget -q --show-progress "$DOWNLOAD_URL"
-chmod +x NVIDIA-Linux-x86_64-"${LATEST_VERSION}".run
-./NVIDIA-Linux-x86_64-"${LATEST_VERSION}".run --extract-only
-echo "🧪 Testando NVIDIA ${LATEST_VERSION} com kernel ${KERNEL_VERSION}..."
-make olddefconfig
-make modules_prepare
-cd NVIDIA-Linux-x86_64-"${LATEST_VERSION}"/kernel-open
-if make -j"$(nproc)" KERNEL_UNAME="${KERNEL_VERSION}" SYSSRC="${KERNEL_SRC_DIR}" modules 2>&1 | tee build.log; then
+sudo apt install -y "$PWD"/linux-rdx*.deb "$PWD"/linux-image*.deb "$PWD"/linux-headers*.deb
+deb_check() {
+	echo "🧪 Testando NVIDIA ${LATEST_VERSION} com kernel ${KERNEL_VERSION}..."
+	sudo dpkg --add-architecture i386
+	wget -q --show-progress "https://developer.download.nvidia.com/compute/cuda/repos/debian13/x86_64/$(curl -sSL https://developer.download.nvidia.com/compute/cuda/repos/debian13/x86_64/ | grep cuda-keyring | cut -d \' -f4)"
+	sudo apt install ./cuda-keyring*.deb
+	rm ./cuda-keyring*.deb
+	sudo apt install -t Debian_13 \
+		libnvidia-encode1 \
+		libvulkan1 \
+		libvulkan1:i386 \
+		nvidia-kernel-open-dkms \
+		nvidia-open \
+		nvidia-settings \
+		nvidia-vaapi-driver \
+		nvtop
+		update-iniramfs -u -k all
+}
+if deb_check ; then
     echo "✅ Compatível"
     exit 0
 else
