1997210a6cb5927f3d7bca2f0e92459b82060907
Raul Dipeas <github@rauldipeas.com.br>
2026-03-02 03:20:54 -0300

    -_config -.editorconfig ^linux-rdx ^linux-source-download

--- scripts/linux-source-download.sh
+++ scripts/linux-source-download.sh
@@ -3,7 +3,7 @@ set -e
 
 # Linux source download
 ## Tarball
-LATEST_KERNEL="$(curl -s https://www.kernel.org/ | grep -A1 'stable:' | grep -oP '\d+\.\d+\.\d+' | head -1)"
+LATEST_KERNEL="$(curl -s https://www.kernel.org/ | grep -A1 'longterm:' | grep -oP '\d+\.\d+\.\d+' | head -1)"
 wget https://cdn.kernel.org/pub/linux/kernel/v"${LATEST_KERNEL%%.*}".x/linux-"${LATEST_KERNEL}".tar.xz
 tar xaf linux-"${LATEST_KERNEL}".tar.xz
 rm linux-"${LATEST_KERNEL}".tar.xz
